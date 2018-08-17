bea8f5305246261b167fa7ce7f770633d3e22485
Jan Schädlich <jan.schaedlich@sensiolabs.de>
2018-08-17 19:26:57 +0200

    Fix phpdoc and constant declaration

--- Iban.php
+++ Iban.php
@@ -24,23 +24,30 @@ namespace Iban\Validation;
  */
 class Iban
 {
-    const LOCALE_CODE_OFFSET = 0;
-    const LOCALE_CODE_LENGTH = 2;
+    public const IBAN_MIN_LENGTH = 15;
 
-    const CHECKSUM_OFFSET = 2;
-    const CHECKSUM_LENGTH = 2;
+    private const LOCALE_CODE_OFFSET = 0;
+    private const LOCALE_CODE_LENGTH = 2;
 
-    const INSTITUTE_IDENTIFICATION_OFFSET = 4;
-    const INSTITUTE_IDENTIFICATION_LENGTH = 8;
+    private const CHECKSUM_OFFSET = 2;
+    private const CHECKSUM_LENGTH = 2;
 
-    const BANKACCOUNT_NUMBER_OFFSET = 12;
-    const BANKACCOUNT_NUMBER_LENGTH = 10;
+    private const INSTITUTE_IDENTIFICATION_OFFSET = 4;
+    private const INSTITUTE_IDENTIFICATION_LENGTH = 8;
 
-    const IBAN_MIN_LENGTH = 15;
-    const ACCOUNT_IDENTIFICATION_OFFSET = 4;
-    
+    private const BANKACCOUNT_NUMBER_OFFSET = 12;
+    private const BANKACCOUNT_NUMBER_LENGTH = 10;
+
+    private const ACCOUNT_IDENTIFICATION_OFFSET = 4;
+
+    /**
+     * @var string
+     */
     private $iban;
 
+    /**
+     * @param string $iban
+     */
     public function __construct($iban)
     {
         $this->iban = $this->normalize($iban);
@@ -51,6 +58,9 @@ class Iban
         return $this->iban;
     }
 
+    /**
+     * @return string
+     */
     public function format()
     {
         return sprintf(
@@ -64,31 +74,50 @@ class Iban
         );
     }
 
+    /**
+     * @return string
+     */
     public function getLocaleCode()
     {
-        return substr($this->iban, Iban::LOCALE_CODE_OFFSET, Iban::LOCALE_CODE_LENGTH);
+        return substr($this->iban, self::LOCALE_CODE_OFFSET, self::LOCALE_CODE_LENGTH);
     }
 
+    /**
+     * @return string
+     */
     public function getChecksum()
     {
-        return substr($this->iban, Iban::CHECKSUM_OFFSET, Iban::CHECKSUM_LENGTH);
+        return substr($this->iban, self::CHECKSUM_OFFSET, self::CHECKSUM_LENGTH);
     }
 
+    /**
+     * @return string
+     */
     public function getAccountIdentification()
     {
-        return substr($this->iban, Iban::ACCOUNT_IDENTIFICATION_OFFSET);
+        return substr($this->iban, self::ACCOUNT_IDENTIFICATION_OFFSET);
     }
 
+    /**
+     * @return string
+     */
     public function getInstituteIdentification()
     {
-        return substr($this->iban, Iban::INSTITUTE_IDENTIFICATION_OFFSET, Iban::INSTITUTE_IDENTIFICATION_LENGTH);
+        return substr($this->iban, self::INSTITUTE_IDENTIFICATION_OFFSET, self::INSTITUTE_IDENTIFICATION_LENGTH);
     }
 
+    /**
+     * @return string
+     */
     public function getBankAccountNumber()
     {
-        return substr($this->iban, Iban::BANKACCOUNT_NUMBER_OFFSET, Iban::BANKACCOUNT_NUMBER_LENGTH);
+        return substr($this->iban, self::BANKACCOUNT_NUMBER_OFFSET, self::BANKACCOUNT_NUMBER_LENGTH);
     }
-    
+
+    /**
+     * @param string $iban
+     * @return string
+     */
     private function normalize($iban)
     {
         return preg_replace('/\s+/', '', trim($iban));
