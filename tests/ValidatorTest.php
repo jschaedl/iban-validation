a3760f51aff62b8feacd193129fd7ff8bb65610e
Jan Schädlich <1880467+jschaedl@users.noreply.github.com>
2023-06-04 16:00:16 +0200

    Apply suggestions from code review

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -245,7 +245,6 @@ final class ValidatorTest extends TestCase
 
         $this->validator->validate(new Iban('DE90 3704 0044 0532 0130 00'), throw: true);
     }
-    
     public function test_validations_are_cleared_before_next_validation(): void
     {
         $isValid = $this->validator->validate(new Iban('DE89 3704 0044 053A 013B 00'));
